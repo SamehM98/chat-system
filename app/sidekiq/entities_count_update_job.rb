6bdbe8ba506428c25752ce2c60d9b14e9d03ea19
Sameh Mohamed <sameh.mohamed@espace.com.eg>
2024-06-12 18:08:26 +0000

    Background job to update entities count

---
+++ app/sidekiq/entities_count_update_job.rb
@@ -0,0 +1,26 @@
+require 'sidekiq-scheduler'
+
+class EntitiesCountUpdateJob
+  include Sidekiq::Worker
+
+  def perform
+    ActiveRecord::Base.transaction do
+      applications = Application.all
+
+      applications.each do |application|
+        application_key = ApplicationHelpers.generate_key(application.token)
+        application.chats_count = InMemoryManager.get(application_key)
+      end
+      applications.each(&:save)
+
+      chats = Chat.all
+
+      chats.find_each do |chat|
+        chat_key = ChatHelpers.generate_key(chat.application_token, chat.chat_number)
+        chat.messages_count = InMemoryManager.get(chat_key)
+      end
+      chats.each(&:save)
+
+    end
+  end
+end
